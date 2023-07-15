c9dae526c540d6036ce22a4ced9c846879117315
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-07-15 13:55:35 +0800

    Progress: Finish Component Categories

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -200,6 +200,52 @@
             </div>
         </section>
         {{-- END CATEGORIES SECTION --}}
+
+
+        {{-- HERO SECTION --}}
+        <section class="hero d-flex align-items-center py-4 py-lg-0 pb-xl-5 pb-xxl-0 position-relative"
+            id="hero">
+            <div class="banner-image position-absolute d-none d-lg-inline-block">
+                <img src="{{ asset('assets/img/banner/hero-banner.svg') }}" class="img-fluid w-100 h-100"
+                    alt="hero Banner Image">
+            </div>
+            <div class="container position-relative">
+                <div class="row align-items-center">
+                    <div class="offset-lg-6 offset-xxl-5 col-lg-6 col-xxl-7">
+                        <h1 class="headline" style="margin-bottom: 16px;">Dive into the World of <span
+                                class="primary">Chroma</span> - Where Art
+                            <span class="primary">Meets Technology</span>
+                        </h1>
+                        <div class="wrapper-paragraph d-flex flex-column gap-2" style="margin-bottom: 36px;">
+                            <p class="paragraph">Welcome to Chroma, the ultimate destination for digital art
+                                enthusiasts
+                                and creators. Our hero section invites you to explore a world where art and technology
+                                intertwine, where imagination knows no bounds, and where colors come alive. Step into a
+                                digital art wonderland where your creativity can flourish and your artistic visions can
+                                be brought to life.</p>
+                            <p class="paragraph d-none d-md-inline-block">Whether you're a seasoned artist or just
+                                starting your creative
+                                journey, Chroma offers a vibrant palette of tools, inspiration, and community to support
+                                and enhance your digital artistry.</p>
+                        </div>
+                        <div class="wrapper d-flex justify-content-between align-items-end">
+                            <div class="wrapper-button d-flex gap-2 align-items-center">
+                                <a href="#" class="button-default">Let’s Explore</a>
+                                <a href="#" class="button-secondary d-flex align-items-center gap-2">
+                                    Our Collection
+                                    <img src="{{ asset('assets/img/icon/arrow-button-dark.svg') }}" class="img-fluid"
+                                        alt="Arrow Button Dark Icon" width="10">
+                                </a>
+                            </div>
+                            <div class="other text-end d-none d-xl-inline-block">
+                                <p class="other-label">scroll down</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{-- END HERO SECTION --}}
     </main>
 
 
