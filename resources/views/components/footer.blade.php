36fc233c07d88b2cb827e774f99da8ee95c34a62
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-22 22:57:07 +0800

    Progress Update Component Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,65 @@
+{{-- FOOTER --}}
+<footer class="footer" id="footer">
+    <div class="container">
+        <div class="row" style="margin-bottom: 100px">
+            <div class="col-6 col-md-3 mb-5 mb-md-0">
+                <a class="footer-brand d-inline-block" href="#home" style="margin-bottom: 18px;">
+                    <img src="{{ asset('assets/img/logo/logo-brand.svg') }}" class="img-fluid w-100" alt="Logo Brand">
+                </a>
+                <p class="paragraph-small" style="margin-bottom: 30px;">Welcome to Chroma, the ultimate
+                    destination for digital art enthusiasts and creators.</p>
+                <div class="wrapper-sosmed d-flex gap-2">
+                    <a href="https://id-id.facebook.com/" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/facebook-icon.svg') }}" class="img-fluid"
+                            alt="Facebook Icon" width="16">
+                    </a>
+                    <a href="https://www.whatsapp.com/" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/whatsapp-icon.svg') }}" class="img-fluid"
+                            alt="Whatsapp Icon" width="16">
+                    </a>
+                    <a href="https://twitter.com/?lang=id" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/twitter-icon.svg') }}" class="img-fluid" alt="Twitter Icon"
+                            width="16">
+                    </a>
+                </div>
+            </div>
+
+            <div class="offset-lg-2 offset-xl-3 col-6 col-md-3 col-lg-2 mb-5 mb-md-0">
+                <h6 style="margin-bottom: 20px">Quick Links</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#home" class="paragraph-small">Home</a>
+                    <a href="#about" class="paragraph-small">About Us</a>
+                    <a href="#adventage" class="paragraph-small">Our Adventage</a>
+                    <a href="#category" class="paragraph-small">Categories Art</a>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-xl-2">
+                <h6 style="margin-bottom: 20px">Adventage Chroma</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#adventage" class="paragraph-small">Unlimited Creative Possibilities</a>
+                    <a href="#adventage" class="paragraph-small">Seamless Workflow Integration</a>
+                    <a href="#adventage" class="paragraph-small">Community and Collaboration</a>
+                    <a href="#adventage" class="paragraph-small">Showcase and Exposure</a>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <h6 style="margin-bottom: 20px">Help & guide</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#" class="paragraph-small">Terms & Conditions</a>
+                    <a href="#" class="paragraph-small">Privacy Policy</a>
+                    <a href="#" class="paragraph-small">Contact Us</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 copyright text-center">
+                <p>Copyright © 2023 Chroma. All Right Reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+{{-- END FOOTER --}}
