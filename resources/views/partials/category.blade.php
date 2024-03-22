570a99289619248cb056bb18115d2977c383e875
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-22 22:57:28 +0800

    Progress Update partials Modal Category

---
+++ resources/views/partials/category.blade.php
@@ -0,0 +1,173 @@
+<div class="modal fade" id="category1" tabindex="-1" aria-labelledby="category1Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Digital Painting</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">This category encompasses artworks that simulate
+                    traditional painting techniques using digital tools and software. It
+                    includes various styles such as realistic, impressionistic, abstract, and
+                    fantasy.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category2" tabindex="-1" aria-labelledby="category2Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Digital Typography and Lettering</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">Artworks that focus on the design and arrangement of
+                    digital typography and letterforms. It includes hand-lettering, calligraphy,
+                    typographic compositions, and experimental typography.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category3" tabindex="-1" aria-labelledby="category3Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Digital Drawing</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">Artworks created primarily using digital drawing
+                    techniques, including sketching, line art, and illustrative styles. It can
+                    range from simple black and white sketches to intricate and detailed
+                    illustrations.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category4" tabindex="-1" aria-labelledby="category4Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Digital Concept Art</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">Artworks created as part of the concept design
+                    process for various media, including video games, movies, and animations.
+                    These artworks often depict characters, environments, and props to visualize
+                    and develop the creative vision.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category5" tabindex="-1" aria-labelledby="category5Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>3D Modeling and Sculpting</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">This category includes artworks that are created
+                    using 3D modeling and sculpting software. Artists use digital tools to
+                    design and manipulate virtual objects, characters, environments, and
+                    sculptures.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category6" tabindex="-1" aria-labelledby="category6Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Digital Animation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">Artworks that involve the creation of moving images
+                    or sequences using digital techniques. This can include 2D animations, 3D
+                    animations, motion graphics, and visual effects.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category7" tabindex="-1" aria-labelledby="category7Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Digital Photography</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">Digital photographs that have been digitally
+                    enhanced, edited, or manipulated fall under this category. It includes
+                    various genres such as landscape, portrait, still life, and conceptual
+                    photography.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="category8" tabindex="-1" aria-labelledby="category8Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Generative Art</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">Artworks that are created using algorithms, code, or
+                    interactive systems to generate visual output. It explores the intersection
+                    of art and technology, often resulting in dynamic and interactive digital
+                    experiences.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-secondary" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
